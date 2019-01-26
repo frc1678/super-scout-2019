8e77cfb938f59c45e9d8eb5e6824c899455ff6d6
thondascully <teohondascully2@gmail.com>
2019-01-26 12:20:23 -0800

    FieldLayout UI and implementation
    Changed view of map in FieldLayout MainActivity

---
+++ app/src/main/java/com/example/sam/blutoothsocketreceiver/Fields/FieldLayout.java
@@ -0,0 +1,11 @@
+package com.example.sam.blutoothsocketreceiver.Fields;
+
+public class FieldLayout {
+
+    public static Integer Blue = -11630645;
+    public static Integer Red = -2594978;
+
+    public static String red = "red";
+    public static String blue = "blue";
+
+}
