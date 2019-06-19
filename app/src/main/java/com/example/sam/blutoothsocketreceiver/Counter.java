d578142a2cc5b14d762463976f94830314a3e02a
Kate Unger <32380357+KateUnger@users.noreply.github.com>
2019-06-18 17:03:28 -0700

    Add comments to Counter file (#50)

--- app/src/main/java/com/example/sam/blutoothsocketreceiver/Counter.java
+++ app/src/main/java/com/example/sam/blutoothsocketreceiver/Counter.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
  * Created by sam on 5/11/16.
  */
 public class Counter extends RelativeLayout {
+    //Declares/"Imports" data points to be used in this class
     private String dataName;
     private int max;
     private int min;
@@ -29,7 +30,7 @@ public class Counter extends RelativeLayout {
 
     public Counter(Context context, AttributeSet attrs) {
         super(context, attrs);
-
+        //Creates a new SuperScoutingPanel and then inflates the Counter layout (xml)
         superScoutingPanel = new SuperScoutingPanel();
 
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -40,14 +41,15 @@ public class Counter extends RelativeLayout {
         addButton = (Button)findViewById(R.id.plusButton);
         subtractButton = (Button)findViewById(R.id.minusButton);
 
-
+        //Creates typed array called "a"
         TypedArray a = context.getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.Counter,
                 0, 0);
 
         try {
-
+            //gets datapoints for each counter (dataName, max, min, startingValue, increment) and puts them into typed array "a". Then, it calls the listenForAddClicked and listenForMinusClicked functions (below). Finally,
+            //the refresh counter is called and then typed array "a" is recycled
             this.dataName = a.getString(R.styleable.Counter_dataName);
             this.max = a.getInt(R.styleable.Counter_max, 4);
             this.min = a.getInt(R.styleable.Counter_min, 0);
@@ -66,6 +68,7 @@ public class Counter extends RelativeLayout {
         }
     }
 
+    //Sets the onClickListener for the "addButton". When clicked, if the value + the increment is less than or equal to the max (all gotten in the Counter class above), then value will be equal to value + increment
     private void listenForAddClicked(){
         addButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -78,6 +81,7 @@ public class Counter extends RelativeLayout {
         });
     }
 
+    //Sets the onClickListener fot the "subtractButton". When clicked, if the value - the increment is greater than or equal to the miniumum (all datapoints gotten in the Counter class above), then value will be equal to value - increment
     private void listenForMinusClicked(){
         subtractButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -90,7 +94,7 @@ public class Counter extends RelativeLayout {
         });
     }
 
-
+    //When called, it will set the counterTitleTextView to the dataName (gotten in the Counter class above), and will set the text of the counterTextView to the someValue integer that was passed in.
     public void refreshCounter(int someValue) { //TODO: Make this private and make a new setCounter method (low priority) (or just refactor).
         value = someValue;
         counterTitleTextView.setText(dataName);
