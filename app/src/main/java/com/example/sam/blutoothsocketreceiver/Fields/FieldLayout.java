a650a0ff19b28759cb04bc3ea927d43521efd53e
Kate Unger <32380357+KateUnger@users.noreply.github.com>
2019-06-30 09:49:44 -0700

    Add Comments to the Files in the Fields Folder (#49)
    * Fields Files Commented
    
    * Fix Spacing and Capitalization of NoShowTeam Button
    
    * Fix comment on color number decimal code
    
    * Update FieldLayout.java

--- app/src/main/java/com/example/sam/blutoothsocketreceiver/Fields/FieldLayout.java
+++ app/src/main/java/com/example/sam/blutoothsocketreceiver/Fields/FieldLayout.java
@@ -2,12 +2,15 @@ package com.example.sam.blutoothsocketreceiver.Fields;
 
 public class FieldLayout {
 
+    //Defines the two alliance color options: red or blue, and the numbers are decimal codes for the colors they represent
     public static Integer Blue = -11630645;
     public static Integer Red = -2594978;
 
+    //Defines the two alliance color options (as Strings): red or blue
     public static String red = "red";
     public static String blue = "blue";
 
+    //Defines the alliance data points: alliance (which alliance: blue or red) and the three teams on the alliance
     public static String alliance = "alliance";
     public static String teamNumberOne = "teamNumberOne";
     public static String teamNumberTwo = "teamNumberTwo";
@@ -19,6 +22,7 @@ public class FieldLayout {
     //gray -2894893
     //violet -3111945
 
+    //Defines the three colors: caused, affected, and neutral, a number (as a String) which is equal to a color
     public static String causedColor = "-2594978";
     public static String affectedColor = "-3111945";
     public static String neutralColor = "-2894893";
